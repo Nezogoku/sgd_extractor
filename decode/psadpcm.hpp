9a0e36fc3f89c23cfee17767da9cdc9dff762895
Cheena <97012270+Nezogoku@users.noreply.github.com>
2022-10-09 20:17:56 -0400

    Update psadpcm.hpp

--- decode/psadpcm.hpp
+++ decode/psadpcm.hpp
@@ -6,6 +6,8 @@
 
 #define SAMPLE_BYTES 14
 #define SAMPLE_NIBBLE SAMPLE_BYTES * 2
+#define SHORT_SAMPLE_BYTES 3
+#define SHORT_SAMPLE_NIBBLE SHORT_SAMPLE_BYTES * 2
 
 //Flags for PS-ADPCM flag byte
 enum flags {
@@ -14,31 +16,51 @@ enum flags {
     LOOP_REGION = 2,     /* Loop region*/
     LOOP_END = 3,        /* Ending block of the loop */
     LOOP_FIRST_BLOCK = 4,/* First block of looped data */
-    UNK = 5,             /* ?*/
+    UNK = 5,             /* Ending position?*/
     LOOP_START = 6,      /* Starting block of the loop*/
     PLAYBACK_END = 7     /* Playback ending position */
 };
 
 //For decoding PS-ADPCM files
-static double vagLut[][2] = {{0.0, 0.0},
-                            {60.0 / 64.0, 0.0},
-                            {115.0 / 64.0, -52.0 / 64.0},
-                            {98.0 / 64.0, -55.0 / 64.0},
-                            {122.0 / 64.0, -60.0 / 64.0}
+const double vagLut[][2] = {
+    {0.0,           0.0},
+    {60.0 / 64.0,   0.0},
+    {115.0 / 64.0,  -52.0 / 64.0},
+    {98.0 / 64.0,   -55.0 / 64.0},
+    {122.0 / 64.0,  -60.0 / 64.0},
+
+    {30.0 / 64.0,   -0.0 / 64.0},
+    {57.5 / 64.0,   -26.0 / 64.0},
+    {49.0 / 64.0,   -27.5 / 64.0},
+    {61.0 / 64.0,   -30.0 / 64.0},
+    {15.0 / 64.0,   -0.0 / 64.0},
+    {28.75/ 64.0,   -13.0 / 64.0},
+    {24.5 / 64.0,   -13.75/ 64.0},
+    {30.5 / 64.0,   -15.0 / 64.0},
+    {32.0 / 64.0,   -60.0 / 64.0},
+    {15.0 / 64.0,   -60.0 / 64.0},
+    {7.0 / 64.0,    -60.0 / 64.0},
 };
 
 //PS-ADPCM chunks
-struct chunk {
-    char shift;
-    char predict;
-    char flag;
-    char data[SAMPLE_BYTES];
+struct vchunk {
+    uint8_t shift;
+    uint8_t predict;
+    uint8_t flag;
+    uint8_t data[SAMPLE_BYTES];
+};
+
+//Short PS-ADPCM chunks
+struct gchunk {
+    uint8_t shift;
+    uint8_t predict;
+    uint8_t data[SHORT_SAMPLE_BYTES];
 };
 
 //Code for decoding header-less PS-ADPCM files
-std::vector<int16_t> adpcmDecode(std::vector<char> adpcmData, uint32_t &lStart, uint32_t &lEnd, bool &looped);
+std::vector<int16_t> adpcmDecode(std::vector<char> adpcmData);
 
 //Code for decoding header-less PS-IMA_ADPCM files
-//std::vector<int16_t> sAdpcmDecode(std::vector<char> sAdpcmData);
+std::vector<int16_t> sAdpcmDecode(std::vector<char> sAdpcmData, int channels);
 
 #endif
