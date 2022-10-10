5207c6f93f4ac09f68ed61b4da92a924c887d532
Cheena <97012270+Nezogoku@users.noreply.github.com>
2022-10-09 20:14:18 -0400

    Update oggvorbis.hpp

--- decode/oggvorbis.hpp
+++ decode/oggvorbis.hpp
@@ -4,7 +4,7 @@
 #include <cstdint>
 #include <vector>
 
-//Code for decoding header-less PS-ADPCM files
-std::vector<int16_t> oggVorbDecode(std::vector<char> oggVorbData);
+//Code for decoding OGG-VORBIS files
+std::vector<int16_t> oggVorbDecode(std::vector<char> oggVorbData, int channels);
 
 #endif
