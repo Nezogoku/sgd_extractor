55cc04cdc4e9b01de7dc6ed946fad33c0dbd7c3a
Cheena <97012270+Nezogoku@users.noreply.github.com>
2022-12-24 12:52:01 -0500

    Update decode.hpp

--- decode.hpp
+++ decode.hpp
@@ -1,9 +1,10 @@
 #ifndef DECODE_HPP
 #define DECODE_HPP
 
+#include "decode/ac3.hpp"
 #include "decode/pcmbe.hpp"
 #include "decode/psadpcm.hpp"
-#include "decode/psatrac.hpp"
+//#include "decode/psatrac.hpp"
 #include "decode/oggvorbis.hpp"
 
 #endif
